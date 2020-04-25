f0f59bea5add165a0ccb64470ca5218f911a3d11
guruprasadlakkond <guruprasadlakkond@gmail.com>
2020-04-25 19:57:38 +0530

    removed unused variables

--- src/services/weatherapi.js
+++ src/services/weatherapi.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?appid=';
-const apiKey = '13fdb39fa4267ca2300edbfdd1e93501';
 const api = axios.create({
   baseURL: 'https://api.openweathermap.org/data/2.5/weather',
 });
